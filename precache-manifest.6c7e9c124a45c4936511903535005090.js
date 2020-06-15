1456b14a759f8386a66d9298cede65aa123c68e3
vforverdu <pablovgdev@gmail.com>
2020-06-15 13:01:50 +0200

    Updates

---
+++ precache-manifest.6c7e9c124a45c4936511903535005090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33648a885f1739798469b0dd94260c88",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "a20f8627dab0dca42e50",
+    "url": "/game-of-life/static/css/main.c4f1f6f5.chunk.css"
+  },
+  {
+    "revision": "7bee4abf5970da082b1e",
+    "url": "/game-of-life/static/js/2.a1662083.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-life/static/js/2.a1662083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a20f8627dab0dca42e50",
+    "url": "/game-of-life/static/js/main.b6b97834.chunk.js"
+  },
+  {
+    "revision": "74c0ec62b28c760df103",
+    "url": "/game-of-life/static/js/runtime-main.ccc8ab68.js"
+  }
+]);
